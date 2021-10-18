dafc62d520a436cac298784ed25eb9b537b45e68
EriqueGreenwood <ghostprotacle@gmail.com>
2021-10-18 16:24:29 -0400

    Created landing page component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import LandingPage from './Components/LandingPage'
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <LandingPage />    
+        </Route>
+      </Switch>
+      <div>
+      </div>
+    </Router>
   )
 }
