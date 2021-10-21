9dc4ad158bea934e28ed1dbe2541018fcc456a9d
mbatista4 <59520635+mbatista4@users.noreply.github.com>
2021-10-21 18:23:29 -0400

    Revert "Adding navbar"

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import LandingPage from './Components/LandingPage'
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <LandingPage />    
-        </Route>
-      </Switch>
-      <div>
-      </div>
-    </Router>
+    <div>
+      
+    </div>
   )
 }
--- src/Components/LandingPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function LandingPage() {
-    return (
-        <div style={{height:"95vh"}}>
-            <h1>Landing Page</h1>
-        </div>
-    )
-}
