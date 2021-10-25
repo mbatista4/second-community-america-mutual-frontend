61eb4e47dbff0ae32567c124d38bef1d76eb20fe
EriqueGreenwood <ghostprotacle@gmail.com>
2021-10-25 15:41:50 -0400

    Finished basic landing page styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+//import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import LandingPage from './Components/LandingPage/LandingPage'
 import "./CSS/App.css"
 
@@ -7,7 +7,7 @@ import "./CSS/App.css"
 export default function App() {
   return (
     <div>
-      
+      <LandingPage />
     </div>
   )
 }
--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -11,7 +11,7 @@ export default function LandingPage() {
             </div>
             <div className="landing-content">
                 <div className="landing-top">
-                <h1>S.C.A.M</h1>
+                <h1 className="title">S.C.A.M</h1>
                 <h3>Open a membership today in just a few minutes</h3>
                 <p>
                     Just complete our online application it's fast, easy, and safe.
