6e396dba9288410a2f0b8fb3e62c6391f425d01a
EriqueGreenwood <ghostprotacle@gmail.com>
2021-11-23 14:10:25 -0500

    sync to master

--- src/Components/LoginPage/LoginPage.jsx
+++ src/Components/LoginPage/LoginPage.jsx
@@ -1,24 +1,53 @@
-import React, {useEffect} from 'react'
+import React, {useEffect,useState} from 'react'
 import { useHistory } from 'react-router';
+import axios from 'axios';
 import "../../CSS/loginpage.css"
 
 
-export default function LoginPage() {
+export default function LoginPage({setLoggedIn}) {
     const history = useHistory();
-    
+    const [errorMsg, setErrorMsg] = useState("");
+    const [loginData, setLoginData] = useState({
+        userId: "",
+        password: ""
+    });
+
     useEffect(() => {
         const token= localStorage.getItem("token");
         if(token && token.length > 1) {
+            setLoggedIn(true);
             history.push("/overview");
         }
     }, []);
+    const handleLoginChange = (e) => {
+        e.preventDefault();
+        const fieldName = e.target.getAttribute("name");
+        const fieldValue = e.target.value;
+        const newFormData = {...loginData};
+        newFormData[fieldName] = fieldValue;
+        setLoginData(newFormData);
+    }
+    const login = async (e) => {
+        e.preventDefault();
+        try {
+            let res = await axios.post(`${process.env.REACT_APP_API_URL}/member/login_member`, loginData);
+            console.log(res)
+            localStorage.setItem("token", res.data)
+            history.push("/overview");
+        } catch (error) {
+            console.log(error.response.data.msg);
+            setErrorMsg(error.response.data.msg);
+        }
+        setLoggedIn(true);
+        
+    }
 
     return (
         <div className="wrapper">
 
-            <form className="login-page ">
-                <input className="input-box-login" name="userId" type="text" placeholder="Username" />
-                <input className="input-box-login" name="password" type="password" placeholder="Password" />
+            <form className="login-page " onSubmit={login}>
+                <input className="input-box-login" name="userId" type="text" placeholder="Username"  onChange={handleLoginChange} required/>
+                <input className="input-box-login" name="password" type="password" placeholder="Password"  onChange={handleLoginChange} required/>
 
                 <div className="btn-box">
                 <button type="submit" className="login-btn">Sign In</button>
